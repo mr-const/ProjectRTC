3b6e37c0e026dbbbe37cdec4aff612f990402dbb
Yevgen Pogribnyi <yevgen.pogribnyi@gmail.com>
2015-11-30 16:50:15 +0200

    Fixed web app client in Firefox Updated code style rules.

--- app/socketHandler.js
+++ app/socketHandler.js
@@ -1,55 +1,55 @@
-module.exports = function(io, streams) {
+module.exports = function (io, streams) {
 
-  io.on('connection', function(client) {
-    console.log('-- ' + client.id + ' joined --');
+    io.on('connection', function (client) {
+        console.log('-- ' + client.id + ' joined --');
 
-    var iceServers = [
-      "stun://stun.l.google.com:19302",
-      "stun://stun1.l.google.com:19302",
-      "stun://stun2.l.google.com:19302",
-      "stun://stun3.l.google.com:19302",
-      "stun://stun4.l.google.com:19302"
-    ]
+        var iceServers = [
+            "stun://stun.l.google.com:19302",
+            "stun://stun1.l.google.com:19302",
+            "stun://stun2.l.google.com:19302",
+            "stun://stun3.l.google.com:19302",
+            "stun://stun4.l.google.com:19302"
+        ]
 
-    var welcomeMessage = {
-      id: client.id,
-      ice_servers: iceServers
-    }
+        var welcomeMessage = {
+            id: client.id,
+            ice_servers: iceServers
+        }
 
-    client.emit('welcome', JSON.stringify(welcomeMessage));
+        client.emit('welcome', JSON.stringify(welcomeMessage));
 
-    client.on('message', function (data) {
-      var message = JSON.parse(data)
+        client.on('message', function (data) {
+            var message = JSON.parse(data)
 
-      var otherClient = io.sockets.connected[message.to];
+            var otherClient = io.sockets.connected[message.to];
 
-      console.log('-- Message from ' + client.id + ' to ' + message.to + ' --')
+            console.log('-- Message from ' + client.id + ' to ' + message.to + ' --')
 
-      if (!otherClient) {
-        return;
-      }
-        delete message.to;
-        message.from = client.id;
-        otherClient.emit('message', JSON.stringify(message));
+            if (!otherClient) {
+                return;
+            }
+            delete message.to;
+            message.from = client.id;
+            otherClient.emit('message', JSON.stringify(message));
+        });
+
+        client.on('register_client', function (data) {
+            var clientDescription = JSON.parse(data)
+            console.log('-- ' + client.id + ' registered with name: ' + clientDescription.name + ' --');
+
+            streams.addStream(client.id, clientDescription.name);
+        });
+
+        client.on('update', function (options) {
+            streams.update(client.id, options.name);
+        });
+
+        function leave() {
+            console.log('-- ' + client.id + ' left --');
+            streams.removeStream(client.id);
+        }
+
+        client.on('disconnect', leave);
+        client.on('leave', leave);
     });
-      
-    client.on('register_client', function(data) {
-      var clientDescription = JSON.parse(data)
-      console.log('-- ' + client.id + ' registered with name: ' + clientDescription.name + ' --');
-      
-      streams.addStream(client.id, clientDescription.name); 
-    });
-    
-    client.on('update', function(options) {
-      streams.update(client.id, options.name);
-    });
-
-    function leave() {
-      console.log('-- ' + client.id + ' left --');
-      streams.removeStream(client.id);
-    }
-
-    client.on('disconnect', leave);
-    client.on('leave', leave);
-  });
 };
--- app/streams.js
+++ app/streams.js
@@ -1,56 +1,56 @@
-module.exports = function() {
-  /**
-   * available streams 
-   * the id value is considered unique (provided by socket.io)
-   */
-  var streamList = [];
+module.exports = function () {
+    /**
+     * available streams
+     * the id value is considered unique (provided by socket.io)
+     */
+    var streamList = [];
 
-  /**
-   * Stream object
-   */
-  var Stream = function(id, name) {
-    this.name = name;
-    this.id = id;
-  }
-
-  return {
-    addStream : function(id, name) {
-      var index = 0;
-      while(index < streamList.length && streamList[index].id != id){
-        index++;
-      }
-      if (index < streamList.length) {
-          console.log('--- Attempt to add duplicate stream with id: ' + id + ' name: ' + name + ' ---')
-          //this.update(id, name)
-          return
-      }
-      var stream = new Stream(id, name);
-      streamList.push(stream);
-      console.log('--- added stream with id: ' + id + ' name: ' + name + ' ---');
-      console.log(streamList);
-    },
-
-    removeStream : function(id) {
-      var index = 0;
-      while(index < streamList.length && streamList[index].id != id){
-        index++;
-      }
-      console.log(streamList);
-      streamList.splice(index, 1);
-      console.log('--- removed stream with id: ' + id + '  index: ' + index);
-      console.log(streamList);
-    },
-
-    // update function
-    update : function(id, name) {
-      var stream = streamList.find(function(element, i, array) {
-        return element.id == id;
-      });
-      stream.name = name;
-    },
-
-    getStreams : function() {
-      return streamList;
+    /**
+     * Stream object
+     */
+    var Stream = function (id, name) {
+        this.name = name;
+        this.id = id;
     }
-  }
+
+    return {
+        addStream: function (id, name) {
+            var index = 0;
+            while (index < streamList.length && streamList[index].id != id) {
+                index++;
+            }
+            if (index < streamList.length) {
+                console.log('--- Attempt to add duplicate stream with id: ' + id + ' name: ' + name + ' ---')
+                //this.update(id, name)
+                return
+            }
+            var stream = new Stream(id, name);
+            streamList.push(stream);
+            console.log('--- added stream with id: ' + id + ' name: ' + name + ' ---');
+            console.log(streamList);
+        },
+
+        removeStream: function (id) {
+            var index = 0;
+            while (index < streamList.length && streamList[index].id != id) {
+                index++;
+            }
+            console.log(streamList);
+            streamList.splice(index, 1);
+            console.log('--- removed stream with id: ' + id + '  index: ' + index);
+            console.log(streamList);
+        },
+
+        // update function
+        update: function (id, name) {
+            var stream = streamList.find(function (element, i, array) {
+                return element.id == id;
+            });
+            stream.name = name;
+        },
+
+        getStreams: function () {
+            return streamList;
+        }
+    }
 };
